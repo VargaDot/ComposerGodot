df7ffe06ee290b9940287c29f7f1db0de11ec07e
SP4R0W <sp4r0wxxx@gmail.com>
2024-12-26 11:39:48 +0100

    Level screen support functionality complete

--- Composer/Composer.gd
+++ Composer/Composer.gd
@@ -4,15 +4,15 @@ signal started_loading(path: String)
 
 signal invalid_scene(path: String)
 signal failed_loading(path: String)
-
 signal updated_loading(path: String, progress: int)
 
 signal finished_loading(scene: Resource)
 signal finished_loading_for_scene()
 
-var _loading_timer: Timer
-var _current_loading_path: String
-var _current_load_settings: LoadSettings
+var _loading_timer: Timer = null
+var _current_loading_path: String = ""
+var _current_load_settings: LoadSettings = null
+var _current_load_screen: Node = null
 
 func _ready() -> void:
 	invalid_scene.connect(_on_invalid_scene)
@@ -30,6 +30,8 @@ func load(path: String, load_settings: LoadSettings = null) -> void:
 	if _loading_timer == null: _setup_timer()
 
 	if load_settings != null: _setup_load(load_settings)
+
+	get_tree().current_scene.queue_free()
 
 	_current_loading_path = path
 	_loading_timer.start()
@@ -65,11 +67,12 @@ func _setup_timer() -> void:
 func _setup_load(load_settings: LoadSettings) -> void:
 	_current_load_settings = load_settings
 
-	var screen: Node = _current_load_settings.loading_screen.instantiate()
-	finished_loading.connect(_current_load_settings.load_screen_event)
+	_current_load_screen = load(_current_load_settings.loading_screen).instantiate()
+	updated_loading.connect(Callable(_current_load_screen, _current_load_settings.load_screen_update_func))
+	finished_loading.connect(Callable(_current_load_screen, _current_load_settings.load_screen_finsh_func))
 
-	get_tree().root.call_deferred("add_child",screen)
-	get_tree().root.call_deferred("move_child",screen, get_child_count()-1)
+	get_tree().root.call_deferred("add_child",_current_load_screen)
+	get_tree().root.call_deferred("move_child",_current_load_screen, get_child_count()-1)
 
 func _on_invalid_scene(path: String) -> void:
 	printerr("Error: Invalid resource: " + path)
@@ -78,16 +81,21 @@ func _on_failed_loading(path: String) -> void:
 	printerr("Error: Failed to load resource: " + path)
 
 func _on_finished_loading(scene: PackedScene) -> void:
-	get_tree().current_scene.queue_free()
-
 	var new_scene: Node = scene.instantiate()
 
 	if _current_load_settings != null:
 		new_scene.hide()
 		new_scene.process_mode = Node.PROCESS_MODE_DISABLED
+		finished_loading_for_scene.connect(Callable(new_scene, _current_load_settings.loaded_scene_start_func))
+
+		if _current_load_settings.call_finished_for_scene:
+			finished_loading_for_scene.emit()
+			_current_load_screen.queue_free()
+
 
 	get_tree().root.call_deferred("add_child", new_scene)
 	get_tree().set_deferred("current_scene", new_scene)
 
-	finished_loading_for_scene.emit()
 	_current_load_settings = null
+	_current_load_screen = null
+	_current_loading_path = ""
