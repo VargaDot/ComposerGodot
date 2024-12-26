70f2b2ae3a1115b8b8d801613c492048b652bf72
SP4R0W <sp4r0wxxx@gmail.com>
2024-12-26 11:43:06 +0100

    Bring back the reverted commit

--- Composer/LoadSettings.gd
+++ Composer/LoadSettings.gd
@@ -1,5 +1,7 @@
 class_name LoadSettings extends Resource
 
-var loading_screen: PackedScene
-var load_screen_event: Callable
-var loaded_scene_func: Callable
+var loading_screen: String
+var load_screen_update_func: String
+var load_screen_finsh_func: String
+var loaded_scene_start_func: String
+var call_finished_for_scene: bool = false
--- Game/game.gd
+++ Game/game.gd
@@ -1,5 +1,9 @@
-extends Node2D
+class_name Game extends Node2D
 
+func on_loading_finished() -> void:
+	show()
+	$CanvasLayer.show()
+	process_mode = Node.PROCESS_MODE_INHERIT
 
 func _on_game_button_pressed() -> void:
 	Composer.load("res://Menu/menu.tscn")
